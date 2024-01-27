ab2da362ad285c9d9c84e88e95c1b298739320f3
Nadim-Mahmud751 <mdnadimmahmud751@gmail.com>
2024-01-27 13:04:42 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,35 @@
+/**
+ * VARIABLE ==> container
+ * */ 
+
+// number
+var sunglassPrice = 80;
+
+// string
+var subject = "CST";
+
+// boolean
+var passed = true;
+
+
+/**
+ * NAMING CONVENTION
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camelCase
+ * */ 
+
+var lastExamResult = 53;
+
+
+/**
+ * OPERATION
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN ?
+ * 4. Null
+ * 5. Number
+ */
